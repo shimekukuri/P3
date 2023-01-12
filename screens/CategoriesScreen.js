ae5a261b1f24aa4b1748b7a63b0e269a1d59f076
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-01-12 01:02:29 -0500

    -

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FlatList } from 'react-native'
+import Category from '../models/category';
+import { CATEGORIES } from '../data/dummy-data';
+
+export default function CategoriesScreen() {
+  return ()
+}
