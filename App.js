25a9ae4d62de3631b54dfd2de672c7105a9aef18
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-01-14 00:29:58 -0500

    -

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Meals Category" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
+
+// <CategoriesScreen />;
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { FlatList } from 'react-native';
-import Category from '../models/category';
-import { NavigationContainer } from '@react-navigation/native';
 import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
-import { StatusBar } from 'expo-status-bar';
 
 function renderCategoryItem(itemData) {
   return (
-    <>
-      <StatusBar style="light" />
-      <CategoryGridTile
-        title={itemData.item.title}
-        color={itemData.item.color}
-      />
-    </>
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
   );
 }
 
